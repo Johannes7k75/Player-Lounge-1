3bbc3f4f2a1867e7803870cbd6a5d036162e411d
Johannes <team.player.lounge@gmail.com>
2021-09-26 20:59:04 +0200

    main

--- commands/infos/abmeldung.js
+++ commands/infos/abmeldung.js
@@ -7,15 +7,15 @@ module.exports = {
     utilisation: '{prefix}abmeldung [date example: 18.8.2021-19.9.2021] [Reason]',
 
     execute(client, message, args) {
-        var unixdate = args[0].split('-')
-        var date = args[0]
-        var reason = args.slice(1).join(' ')
+        var unixdate = args[0].split('-');
+        var date = args[0];
+        var reason = args.slice(1).join(' ');
 
-        unixdate = unixdate[1].split('.')
+        unixdate = unixdate[1].split('.');
         // 11.8.2021
-        unixdate = new Date(unixdate[2], unixdate[1] - 1, unixdate[0]).getTime()
+        unixdate = new Date(unixdate[2], unixdate[1] - 1, unixdate[0]).getTime();
 
-        message.delete()
-        message.channel.send({ embed: { title: `Abmeldung`, description: `**${message.author.username}#${message.author.discriminator}** Meldet sich vom **${date}** ab.\nGrund: **${reason}**\n\nAlso bis <t:${unixdate / 1000}:R>! :wave:` } })
+        message.delete();
+        message.channel.send({ embed: { title: `Abmeldung`, description: `**${message.author.tag}** Meldet sich vom **${date}** ab.\nGrund: **${reason}**\n\nAlso bis <t:${unixdate / 1000}:R>! :wave:` } });
     },
 };

797326a2930c03cbcd9eb692161df1c7d84295dd
player-lounge-dev <team.player.lounge@gmail.com>
2021-10-10 17:30:53 +0200

    main

--- commands/dev/debug.js
+++ commands/dev/debug.js
@@ -8,21 +8,21 @@ module.exports = {
     async execute(client, message) {
         const discord = require('discord.js');
         let embed = new discord.MessageEmbed()
-            .setTitle('Test1')
+            .setTitle('Rollen Informationen/Aufgaben')
             .addFields(
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
-                { name: 'title1', value: 'value1' },
+                { name: 'Owner', value: 'value1' },
+                { name: 'Co.Owner', value: 'value1' },
+                { name: 'Admin', value: 'value1' },
+                { name: 'Co.Admin', value: 'value1' },
+                { name: 'Moderator', value: 'value1' },
+                { name: 'Co.Moderator', value: 'value1' },
+                { name: 'Supporter', value: 'value1' },
+                { name: 'Co.Supporter', value: 'value1' },
+                { name: 'Developer', value: 'value1' },
+                { name: 'Designer', value: 'value1' },
+                { name: 'Slaves', value: 'value1' },
+                { name: 'Team', value: 'value1' },
+                { name: 'VIP', value: 'value1' },
                 { name: 'title1', value: 'value1' },
                 { name: 'title1', value: 'value1' },
                 { name: 'title1', value: 'value1' },
